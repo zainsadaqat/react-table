ca61ecfb7e5200620f04438bd8f68ab3c0280a71
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-11-13 23:57:25 +0500

    Create a basic table

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import FilteringTable from "./components/filtering-table";
+import PaginationTable from "./components/pagination-table";
+import SortingTable from "./components/sorting-table";
+
+function App() {
+  return (
+    <>
+      {/* <SortingTable /> */}
+      {/* <FilteringTable /> */}
+      <PaginationTable />
+    </>
+  );
+}
+
+export default App;
