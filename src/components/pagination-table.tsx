71705ec0d564161f578571dcab749ac3f045be92
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-11-13 23:58:06 +0500

    Add pagination table

---
+++ src/components/pagination-table.tsx
@@ -0,0 +1,109 @@
+import { useTable, usePagination } from "react-table";
+import MockData from "../data/mock-data.json";
+import { COLUMNS } from "../constants/columns";
+import { useMemo } from "react";
+import { Container } from "react-bootstrap";
+
+const PaginationTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MockData, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    canNextPage,
+    previousPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+    setPageSize,
+    gotoPage,
+    pageCount,
+    prepareRow,
+  } =
+    // @ts-expect-error "error on the columns param"
+    useTable({ data, columns }, usePagination);
+
+  const { pageIndex, pageSize } = state;
+
+  return (
+    <Container>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => {
+            return (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            );
+          })}
+        </select>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
+      </div>
+    </Container>
+  );
+};
+
+export default PaginationTable;
